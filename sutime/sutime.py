863dc058a324add1a39c7a20bf561fcc8626b0a2
Mohit M <mohitcrox@gmail.com>
2017-08-03 22:01:12 +0530

    mc

--- sutime/sutime.py
+++ sutime/sutime.py
@@ -64,7 +64,7 @@ class SUTime(object):
     def _start_jvm(self):
         if jpype.isJVMStarted() is not 1:
             jpype.startJVM(
-                jpype.getDefaultJVMPath(), "-mx500m", 
+                jpype.getDefaultJVMPath(), "-mx1g", 
                 '-Djava.class.path={classpath}'.format(
                     classpath=self._classpath)
             )
