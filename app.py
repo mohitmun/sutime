4013c0f749ec5ae73aedcc7cd476e4bcf3e1bd24
Mohit M <mohitcrox@gmail.com>
2017-08-03 16:43:43 +0530

    mc

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask
 from datetime import datetime
 import example
 app = Flask(__name__)
-
+import json
 @app.route('/')
 def homepage():
   the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-  return 
+  q = request.args.get('q')
+  return json.dumps(example.parse(q))
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
