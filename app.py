48465087f8b12a1dedc110c99f59ef87d0928920
Mohit M <mohitcrox@gmail.com>
2017-08-03 16:58:37 +0530

    mc

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import request
 import example
-app = Flask(__name__)
+import os
 import json
+from sutime import SUTime
+import sys
+import json
+app = Flask(__name__)
+jar_files = os.path.join(os.path.dirname(__file__), 'jars')
+sutime = SUTime(jars=jar_files, mark_time_ranges=False)
 @app.route('/')
 def homepage():
   q = request.args.get('q')
-  return json.dumps(example.parse(q))
-
+  return json.dumps(parse(q))
+def parse(s):
+  return sutime.parse(s)
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
